4d889e1c58f93fc9f09139535c4a8ee04ce7aa1e
yogiiiaulia <yogiauliaardiano@gmail.com>
2025-05-27 20:24:14 +0700

    selesai

--- Komposisi/manusia.h
+++ Komposisi/manusia.h
@@ -4,4 +4,17 @@
 using namespace std;
 
 class manusia {
+public:
+    string name;
+    jantung varJantung;
+
+    manusia(string pName)
+    : name(pName) {
+        cout <<  name << "hidup\n";
+    }
+    ~manusia(){
+        cout << name << "mati\n";
+        }
+};
+#endif
 
