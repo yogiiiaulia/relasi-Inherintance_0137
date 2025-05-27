2ca543726ba72a78b3fec8f05e2cc17b3f9eacf7
yogiiiaulia <yogiauliaardiano@gmail.com>
2025-05-27 20:17:42 +0700

    pembuatan class anak

--- Agregasi/anak.h
+++ Agregasi/anak.h
@@ -3,14 +3,4 @@
 
 class anak {
     public:
-    string nama;
-    anak(string pNama): nama (pNama) {
-        cout << "Anak \"" << nama << "\"ada\n";
-
-    }
-
-    ~anak() {
-        cout << "Anak \"" << nama << "\"tidak ada\n";
-    }
-};
-#endif
+    
