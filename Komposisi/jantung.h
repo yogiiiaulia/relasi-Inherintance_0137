8b9b9327850a757561b3093c6279cd81d1f38985
yogiiiaulia <yogiauliaardiano@gmail.com>
2025-05-27 20:23:22 +0700

    selesai

--- Komposisi/jantung.h
+++ Komposisi/jantung.h
@@ -5,4 +5,13 @@
 using namespace std;
 
 class jantung{
-   
+    public:
+        jantung(){
+            cout << "jantung dihidupkan\n";
+        }
+        ~jantung(){
+            cout << "jantung dimatikan\n";
+        }
+};
+#endif
+
