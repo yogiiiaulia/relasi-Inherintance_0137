e96e2fd959b13cabda07f15aa8ae11ecfc50bba7
yogiiiaulia <yogiauliaardiano@gmail.com>
2025-05-27 20:18:51 +0700

    pembuatan class ibu

--- Agregasi/ibu.h
+++ Agregasi/ibu.h
@@ -3,28 +3,4 @@
 
 class ibu{
     public:
-        string nama;
-        vector<anak*> daftar_anak;
-
-        ibu(string pNama): nama(pNama){
-            cout << "ibu \"" << nama << "\" ada\n";
-        }
-
-        ~ibu() {
-            cout << "ibu\"" << nama << "\" tidak ada\n";
-        }
-        void tambahAnak (anak*);
-        void cetakAnak();
-};
-void ibu::tambahAnak(anak* pAnak){
-    daftar_anak.push_back(pAnak);
-}
-void ibu::cetakAnak(){
-    cout << "Daftar anak dari ibu \"" << 
-    this->nama << "\":\n";
-    for (int i = 0; i < daftar_anak.size(); i++){
-        cout << daftar_anak[i]->nama << endl;
-    }
-    cout << endl;
-}
-#endif
+       
